71e591ca0f126fb2ad038908ee05d555444c2b57
Matevz Tadel <mtadel@ucsd.edu>
2021-10-01 11:40:37 -0700

    Rewrite seed-partition function to also take into account TID crossing.

--- RecoTracker/MkFit/plugins/MkFitIterationConfigESProducer.cc
+++ RecoTracker/MkFit/plugins/MkFitIterationConfigESProducer.cc
@@ -29,8 +29,6 @@ namespace {
       const bool z_dir_pos = S.pz() > 0;
 
       HitOnTrack hot = S.getLastHitOnTrack();
-      // MIMI ACHTUNG -- here we assume seed hits have already been remapped.
-      // This was true at that time :)
       float eta = eoh[hot.layer].GetHit(hot.index).eta();
       // float  eta = S.momEta();
 
@@ -100,6 +98,93 @@ namespace {
       part.m_sort_score[i] = 5.0f * (reg - 2) + eta;
     }
   }
+
+  void partitionSeeds1(const TrackerInfo &trk_info,
+                       const TrackVec &in_seeds,
+                       const EventOfHits &eoh,
+                       IterationSeedPartition &part) {
+    // Seeds are placed into eta regions and sorted on region + eta.
+
+    const LayerInfo &tib1 = trk_info.m_layers[4];
+    //const LayerInfo &tib6 = trk_info.m_layers[9];
+    const LayerInfo &tob1 = trk_info.m_layers[10];
+    //const LayerInfo &tob8 = trk_info.m_layers[17];
+
+    const LayerInfo &tidp1 = trk_info.m_layers[21];
+    const LayerInfo &tidn1 = trk_info.m_layers[48];
+
+    const LayerInfo &tecp1 = trk_info.m_layers[27];
+    const LayerInfo &tecn1 = trk_info.m_layers[54];
+
+    // Merge first two layers to account for mono/stereo coverage.
+    // TrackerInfo could hold joint limits for sub-detectors.
+    const auto &L = trk_info.m_layers;
+    const float tidp_rin  = std::min(L[21].m_rin,  L[22].m_rin);
+    const float tidn_rin  = std::min(L[48].m_rin,  L[49].m_rin);
+    const float tecp_rin  = std::min(L[27].m_rin,  L[28].m_rin);
+    const float tecn_rin  = std::min(L[54].m_rin,  L[55].m_rin);
+    const float tidp_rout = std::max(L[21].m_rout, L[22].m_rout);
+    const float tidn_rout = std::max(L[48].m_rout, L[49].m_rout);
+    const float tecp_rout = std::max(L[27].m_rout, L[28].m_rout);
+    const float tecn_rout = std::max(L[54].m_rout, L[55].m_rout);
+
+    const int size = in_seeds.size();
+
+    for (int i = 0; i < size; ++i) {
+      const Track &S = in_seeds[i];
+
+      HitOnTrack hot = S.getLastHitOnTrack();
+      float eta = eoh[hot.layer].GetHit(hot.index).eta();
+      // float  eta = S.momEta();
+
+      // Region to be defined by propagation / intersection tests
+      TrackerInfo::EtaRegion reg;
+
+      const bool  z_dir_pos = S.pz() > 0;
+      const float maxR = S.maxReachRadius();
+
+      if (z_dir_pos) {
+        bool in_tib = maxR > tib1.m_rin && S.zAtR(tib1.m_rin) < tib1.m_zmax;
+        bool in_tob = maxR > tob1.m_rin && S.zAtR(tob1.m_rin) < tob1.m_zmax;
+        if (!in_tib && !in_tob) {
+          reg = TrackerInfo::Reg_Endcap_Pos;
+        } else {
+          const LayerInfo &tid1 = tidp1;
+          const LayerInfo &tec1 = tecp1;
+          bool in_tid = maxR > tidp_rout ?  S.zAtR(tidp_rout) > tid1.m_zmin :
+                       (maxR > tidp_rin  && S.zAtR(maxR)      > tid1.m_zmin);
+          bool in_tec = maxR > tecp_rout ?  S.zAtR(tecp_rout) > tec1.m_zmin :
+                       (maxR > tecp_rin  && S.zAtR(maxR)      > tec1.m_zmin);
+
+          if (!in_tid && !in_tec)
+            reg = TrackerInfo::Reg_Barrel;
+          else
+            reg = TrackerInfo::Reg_Transition_Pos;
+        }
+      } else {
+        bool in_tib = maxR > tib1.m_rin && S.zAtR(tib1.m_rin) > tib1.m_zmin;
+        bool in_tob = maxR > tob1.m_rin && S.zAtR(tob1.m_rin) > tob1.m_zmin;
+        if (!in_tib && !in_tob) {
+          reg = TrackerInfo::Reg_Endcap_Neg;
+        } else {
+          const LayerInfo &tid1 = tidn1;
+          const LayerInfo &tec1 = tecn1;
+          bool in_tid = maxR > tidn_rout ?  S.zAtR(tidn_rout) < tid1.m_zmax :
+                       (maxR > tidn_rin  && S.zAtR(maxR)      < tid1.m_zmax);
+          bool in_tec = maxR > tecn_rout ?  S.zAtR(tecn_rout) < tec1.m_zmax :
+                       (maxR > tecn_rin  && S.zAtR(maxR)      < tec1.m_zmax);
+
+          if (!in_tid && !in_tec)
+            reg = TrackerInfo::Reg_Barrel;
+          else
+            reg = TrackerInfo::Reg_Transition_Neg;
+        }
+      }
+
+      part.m_region[i] = reg;
+      part.m_sort_score[i] = 8.0f * (reg - 2) + eta;
+    }
+  }
 }  // namespace
 
 
@@ -130,7 +215,7 @@ void MkFitIterationConfigESProducer::fillDescriptions(edm::ConfigurationDescript
 std::unique_ptr<mkfit::IterationConfig> MkFitIterationConfigESProducer::produce(
     const TrackerRecoGeometryRecord& iRecord) {
   auto it_conf = mkfit::ConfigJson_Load_File(configFile_);
-  it_conf->m_partition_seeds = partitionSeeds0;
+  it_conf->m_partition_seeds = partitionSeeds1;
   return it_conf;
 }
 
